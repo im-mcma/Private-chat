cb82d6dd04c6df2b8847dd7d22b48e9f8dd3d09b
𝕚𝕞_𝕒𝕓𝕚 <im.abi.cma@gmail.com>
2025-06-02 01:13:44 +0330

    Delete admin directory

--- admin/main.py
+++
@@ -1,8 +0,0 @@
-@routes.route('/admin')
-@login_required
-def admin_panel():
-    if current_user.username != 'admin':
-        abort(403)
-    users = User.query.all()
-    messages = Message.query.order_by(Message.timestamp.desc()).all()
-    return render_template('admin.html', users=users, messages=messages) 
